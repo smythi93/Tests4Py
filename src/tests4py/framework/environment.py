27e3e228a3a8f40ddfb784e9b7d4f807a49af15a
Marius Smytzek <marius.smytzek@cispa.de>
2023-04-23 00:02:01 +0200

    fix vor versions

--- src/tests4py/framework/environment.py
+++ src/tests4py/framework/environment.py
@@ -3,7 +3,6 @@ import shutil
 import subprocess
 import sys
 from pathlib import Path
-from typing import List
 
 from tests4py.framework.constants import Environment, VERSION_PATTERN, VENV
 from tests4py.framework.logger import LOGGER
@@ -31,26 +30,39 @@ def __install_version__(project: Project):
         if project.python_version and v != project.python_fallback_version:
             LOGGER.info(f"Failed. Check for fallback version to pyenv python {v}")
             try:
-                LOGGER.info(f"Try to install pyenv python {v}")
-                subprocess.check_call(["pyenv", "install", v])
+                LOGGER.info(
+                    f"Try to install pyenv python {project.python_fallback_version}"
+                )
+                subprocess.check_call(
+                    ["pyenv", "install", project.python_fallback_version]
+                )
                 return v
             except subprocess.CalledProcessError:
                 pass
         LOGGER.info(f"Failed. Check for best alternative to pyenv python {v}")
-        v = ".".join(project.python_version.split(".")[:-1])
         p, r, s = project.python_version.split(".")[:3]
+        r = int(r)
         s = int(s)
-        best_sub_version = None
+        best_r = None
+        best_s = None
         for candidate in current_versions:
             cp, cr, cs = candidate.split(".")[:3]
-            if cp == p and cr == r:
-                cs = int(cs)
-                if cs >= s and best_sub_version is None or cs < best_sub_version:
-                    best_sub_version = cs
-        if best_sub_version is not None:
-            v = f"{v}.{best_sub_version}"
+            if cp == p:
+                cr = int(cr)
+                if cr > r:
+                    if best_r is None or cr < best_r or (cr == best_r and cs < best_s):
+                        best_r = r
+                        best_s = s
+                elif cr == r:
+                    cs = int(cs)
+                    if cs >= s and (best_s is None or cs < best_s):
+                        best_r = r
+                        best_s = s
+        if best_r and best_s:
+            v = f"{p}.{best_r}.{best_s}"
             LOGGER.info(f"Found alternative to pyenv python {v}")
             return v
+        v = f"{p}.{r}"
         LOGGER.info(f"Failed. Try to install fallback pyenv python {v}")
         try:
             output = subprocess.check_output(["pyenv", "install", v]).decode("utf-8")
@@ -63,7 +75,7 @@ def __install_version__(project: Project):
                 LOGGER.info(f"Failed. Using latest python version instead.")
                 return current_versions[-1]
         except subprocess.CalledProcessError:
-            LOGGER.info(f"Failed. Using lates python version instead.")
+            LOGGER.info(f"Failed. Using latest python version instead.")
             return current_versions[-1]
     else:
         return v
@@ -85,7 +97,7 @@ def __env_on__(project: Project, skip=False) -> Environment:
         .decode("utf-8")
         .replace("\n", "")
     )
-    if not output.endswith(v):
+    if v not in output:
         raise OSError(
             f"Python version {v} not set, because current version is {output}"
         )
