96a568485ca0a0a0f6b226834f19693357783221
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-04-26 09:53:07 +0200

    Revert "youtubedl"
    This reverts commit c2626f811b25570d518ba8fcaf5e8ccef5510080.

--- src/tests4py/projects/youtubedl.py
+++ src/tests4py/projects/youtubedl.py
@@ -27,7 +27,7 @@ class YoutubeDL(Project):
     ):
         super().__init__(
             bug_id=bug_id,
-            project_name="youtubedl",
+            project_name="youtube-dl",
             github_url="https://github.com/ytdl-org/youtube-dl",
             status=Status.OK,
             cause="N.A.",
@@ -35,7 +35,7 @@ class YoutubeDL(Project):
             python_path=python_path,
             buggy_commit_id=buggy_commit_id,
             fixed_commit_id=fixed_commit_id,
-            testing_framework=TestingFramework.UNITTEST,
+            testing_framework=TestingFramework.PYTEST,
             test_file=test_file,
             test_cases=test_cases,
             darwin_python_version=darwin_python_version,
@@ -50,60 +50,9 @@ class YoutubeDL(Project):
 
 
 def register():
-    YoutubeDL(
-        bug_id=1,
-        python_version='3.7.0',
-        darwin_python_version='3.7.12',
-        python_path='',
-        buggy_commit_id='99036a1298089068dcf80c0985bfcc3f8c24f281',
-        fixed_commit_id='1cc47c667419e0eadc0a6989256ab7b276852adf',
-        test_file=[Path('test', 'test_utils.py')],
-        test_cases=['test.test_utils.TestUtil.test_match_str']
-    )
+    # TODO implement the 43 bugs of youtube-dl
+    pass
 
-    YoutubeDL(
-        bug_id=2,
-        python_version='3.7.0',
-        darwin_python_version='3.7.12',
-        python_path='',
-        buggy_commit_id='84f085d4bdb66ee025fb337bcd571eab7469da97',
-        fixed_commit_id='9d6ac71c27b1dfb662c795ef598dbfd0286682da',
-        test_file=[Path('test', 'test_InfoExtractor.py')],
-        test_cases=['test.test_InfoExtractor.TestInfoExtractor.test_parse_mpd_formats']
-    )
-
-    YoutubeDL(
-        bug_id=3,
-        python_version='3.7.0',
-        darwin_python_version='3.7.12',
-        python_path='',
-        buggy_commit_id='f5469da9e6e259c1690c7ef54f1da1c19f65036f',
-        fixed_commit_id='95f3f7c20a05e7ac490e768b8470b20538ef8581',
-        test_file=[Path('test', 'test_utils.py')],
-        test_cases=['test.test_utils.TestUtil.test_unescape_html']
-    )
-
-    YoutubeDL(
-        bug_id=4,
-        python_version='3.7.0',
-        darwin_python_version='3.7.12',
-        python_path='',
-        buggy_commit_id='bc40b3a5ba44006c23daf7fe0ed872af5e33bdc5',
-        fixed_commit_id='189935f15960300d316e8b07108b076ac6c2186a',
-        test_file=[Path('test', 'test_jsinterp.py')],
-        test_cases=['test.test_jsinterp.TestJSInterpreter.test_call']
-    )
-
-    YoutubeDL(
-        bug_id=43,
-        python_version='3.7.4',
-        darwin_python_version='3.7.12',
-        python_path='',
-        buggy_commit_id='cecaaf3f58ad9f544dbb79af1e565d9353fa2b2d',
-        fixed_commit_id='d6c7a367e88096bb17e323954002c084477fe908',
-        test_file=[Path('test', 'test_utils.py')],
-        test_cases=['test.test_utils.TestUtil.test_url_basename']
-    )
 
 class YoutubeDLAPI(API):
     def __init__(self, default_timeout: int = 5):
