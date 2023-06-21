6afbaf47c401fceed5080d2b8ed01a6a1bf65556
Marius Smytzek <marius.smytzek@cispa.de>
2023-06-21 08:30:36 +0200

    updated test for import

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -3,7 +3,7 @@ import os
 
 from fuzzingbook import Grammars
 
-import tests4py.framework.constants
+import tests4py.constants
 import tests4py.framework.default
 from tests4py import framework
 from tests4py.projects import load_bug_info
@@ -15,21 +15,19 @@ class CommandTests(BaseTest):
         report = tests4py.framework.default.tests4py_checkout("pysnooper", 1)
         if report.raised:
             raise report.raised
-        self.assertTrue(
-            (tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_1").exists()
-        )
+        self.assertTrue((tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_1").exists())
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_1"
-                / tests4py.framework.constants.INFO_FILE
+                / tests4py.constants.INFO_FILE
             ).exists()
         )
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_1"
-                / tests4py.framework.constants.REQUIREMENTS_FILE
+                / tests4py.constants.REQUIREMENTS_FILE
             ).exists()
         )
 
@@ -37,44 +35,42 @@ class CommandTests(BaseTest):
         report = tests4py.framework.default.tests4py_checkout("pysnooper", 2)
         if report.raised:
             raise report.raised
-        self.assertTrue(
-            (tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_2").exists()
-        )
+        self.assertTrue((tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_2").exists())
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_2"
-                / tests4py.framework.constants.INFO_FILE
+                / tests4py.constants.INFO_FILE
             ).exists()
         )
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_2"
-                / tests4py.framework.constants.REQUIREMENTS_FILE
+                / tests4py.constants.REQUIREMENTS_FILE
             ).exists()
         )
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_2"
-                / tests4py.framework.constants.DEFAULT_UNITTESTS_DIVERSITY_PATH
+                / tests4py.constants.DEFAULT_UNITTESTS_DIVERSITY_PATH
             ).exists()
         )
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_2"
-                / tests4py.framework.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
+                / tests4py.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
             ).exists()
         )
         self.assertEqual(
             20,
             len(
                 os.listdir(
-                    tests4py.framework.constants.DEFAULT_WORK_DIR
+                    tests4py.constants.DEFAULT_WORK_DIR
                     / "pysnooper_2"
-                    / tests4py.framework.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
+                    / tests4py.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
                 )
             ),
         )
@@ -83,44 +79,42 @@ class CommandTests(BaseTest):
         report = tests4py.framework.default.tests4py_checkout("pysnooper", 3)
         if report.raised:
             raise report.raised
-        self.assertTrue(
-            (tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3").exists()
-        )
+        self.assertTrue((tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3").exists())
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_3"
-                / tests4py.framework.constants.INFO_FILE
+                / tests4py.constants.INFO_FILE
             ).exists()
         )
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_3"
-                / tests4py.framework.constants.REQUIREMENTS_FILE
+                / tests4py.constants.REQUIREMENTS_FILE
             ).exists()
         )
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_3"
-                / tests4py.framework.constants.DEFAULT_UNITTESTS_DIVERSITY_PATH
+                / tests4py.constants.DEFAULT_UNITTESTS_DIVERSITY_PATH
             ).exists()
         )
         self.assertTrue(
             (
-                tests4py.framework.constants.DEFAULT_WORK_DIR
+                tests4py.constants.DEFAULT_WORK_DIR
                 / "pysnooper_3"
-                / tests4py.framework.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
+                / tests4py.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
             ).exists()
         )
         self.assertEqual(
             20,
             len(
                 os.listdir(
-                    tests4py.framework.constants.DEFAULT_WORK_DIR
+                    tests4py.constants.DEFAULT_WORK_DIR
                     / "pysnooper_3"
-                    / tests4py.framework.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
+                    / tests4py.constants.DEFAULT_SYSTEMTESTS_DIVERSITY_PATH
                 )
             ),
         )
@@ -129,22 +123,22 @@ class CommandTests(BaseTest):
         report = tests4py.framework.default.tests4py_checkout("pysnooper", 1)
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_1"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_1"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
 
     def test_test_pysnooper_3(self):
         tests4py.framework.default.tests4py_checkout("pysnooper", 3, fixed=False)
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         tests4py.framework.default.tests4py_compile(work_dir)
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = tests4py.framework.default.tests4py_test(work_dir)
         if report.raised:
@@ -159,13 +153,13 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.unittest.tests4py_test(work_dir, diversity=True)
         if report.raised:
@@ -180,13 +174,13 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.unittest.tests4py_test(work_dir, diversity=True)
         if report.raised:
@@ -201,13 +195,13 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.systemtest.tests4py_test(work_dir, diversity=True)
         if report.raised:
@@ -222,13 +216,13 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.systemtest.tests4py_test(work_dir, diversity=True)
         if report.raised:
@@ -243,28 +237,26 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.unittest.tests4py_generate(work_dir, n=6, p=0.5)
         if report.raised:
             raise report.raised
         self.assertTrue(
-            (
-                work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_UNITTESTS
-            ).exists()
+            (work_dir / tests4py.constants.DEFAULT_SUB_PATH_UNITTESTS).exists()
         )
         self.assertEqual(6, report.total)
         self.assertEqual(3, report.failing)
         self.assertEqual(3, report.passing)
         report = framework.unittest.tests4py_test(
             work_dir,
-            path=work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_UNITTESTS,
+            path=work_dir / tests4py.constants.DEFAULT_SUB_PATH_UNITTESTS,
             diversity=False,
         )
         if report.raised:
@@ -279,28 +271,26 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.unittest.tests4py_generate(work_dir, n=6, p=0.5)
         if report.raised:
             raise report.raised
         self.assertTrue(
-            (
-                work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_UNITTESTS
-            ).exists()
+            (work_dir / tests4py.constants.DEFAULT_SUB_PATH_UNITTESTS).exists()
         )
         self.assertEqual(6, report.total)
         self.assertEqual(3, report.failing)
         self.assertEqual(3, report.passing)
         report = framework.unittest.tests4py_test(
             work_dir,
-            path=work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_UNITTESTS,
+            path=work_dir / tests4py.constants.DEFAULT_SUB_PATH_UNITTESTS,
             diversity=False,
         )
         if report.raised:
@@ -315,28 +305,26 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.systemtest.tests4py_generate(work_dir, n=6, p=0.5)
         if report.raised:
             raise report.raised
         self.assertTrue(
-            (
-                work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_SYSTEMTESTS
-            ).exists()
+            (work_dir / tests4py.constants.DEFAULT_SUB_PATH_SYSTEMTESTS).exists()
         )
         self.assertEqual(6, report.total)
         self.assertEqual(3, report.failing)
         self.assertEqual(3, report.passing)
         report = framework.systemtest.tests4py_test(
             work_dir,
-            path=work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_SYSTEMTESTS,
+            path=work_dir / tests4py.constants.DEFAULT_SUB_PATH_SYSTEMTESTS,
             diversity=False,
         )
         if report.raised:
@@ -351,28 +339,26 @@ class CommandTests(BaseTest):
         )
         if report.raised:
             raise report.raised
-        work_dir = tests4py.framework.constants.DEFAULT_WORK_DIR / "pysnooper_3"
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "pysnooper_3"
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertFalse(project.compiled)
         report = tests4py.framework.default.tests4py_compile(work_dir)
         if report.raised:
             raise report.raised
-        project = load_bug_info(work_dir / tests4py.framework.constants.INFO_FILE)
+        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
         self.assertTrue(project.compiled)
         report = framework.systemtest.tests4py_generate(work_dir, n=6, p=0.5)
         if report.raised:
             raise report.raised
         self.assertTrue(
-            (
-                work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_SYSTEMTESTS
-            ).exists()
+            (work_dir / tests4py.constants.DEFAULT_SUB_PATH_SYSTEMTESTS).exists()
         )
         self.assertEqual(6, report.total)
         self.assertEqual(3, report.failing)
         self.assertEqual(3, report.passing)
         report = framework.systemtest.tests4py_test(
             work_dir,
-            path=work_dir / tests4py.framework.constants.DEFAULT_SUB_PATH_SYSTEMTESTS,
+            path=work_dir / tests4py.constants.DEFAULT_SUB_PATH_SYSTEMTESTS,
             diversity=False,
         )
         if report.raised:
@@ -405,7 +391,7 @@ class CommandTests(BaseTest):
 
     def test_grammar_antlr(self):
         file = "grammar.g4"
-        target = "test_antlr"
+        # target = "test_antlr"
         tests4py.framework.grammar.tests4py_grammar(
             "pysnooper", 2, grammar_format="antlr", output=file
         )
