ae4c4b6cc0aa674cb0297ab5d4ace4ec1d921156
Marius Smytzek <marius.smytzek@cispa.de>
2023-08-21 13:52:04 +0200

    Tests for run

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -465,22 +465,3 @@ class CommandTests(BaseTest):
         if report.raised:
             raise report.raised
         self.assertEqual(TestResult.FAILING, report.test_result)
-
-    def test_run_middle_2(self):
-        report = tests4py.framework.default.tests4py_checkout("middle", 2, fixed=False)
-        if report.raised:
-            raise report.raised
-        work_dir = tests4py.constants.DEFAULT_WORK_DIR / "middle_2"
-        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
-        self.assertFalse(project.compiled)
-        report = tests4py.framework.default.tests4py_compile(work_dir)
-        if report.raised:
-            raise report.raised
-        project = load_bug_info(work_dir / tests4py.constants.INFO_FILE)
-        self.assertTrue(project.compiled)
-        report = framework.systemtest.tests4py_run(
-            work_dir, inputs=["2", "1", "3"], invoke_oracle=True
-        )
-        if report.raised:
-            raise report.raised
-        self.assertEqual(TestResult.FAILING, report.test_result)
