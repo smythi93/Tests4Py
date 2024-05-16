a95c8565c145ea3efe045f7d7279294941abe445
Marius Smytzek <marius.smytzek@cispa.de>
2024-05-16 11:23:44 +0200

    fixed system tests

--- src/tests4py/projects/expression.py
+++ src/tests4py/projects/expression.py
@@ -299,7 +299,7 @@ grammar: Grammar = {
         "<integers_> <symbol_> (<integers_> <symbol_> <integers_>)",
     ],
     "<symbol_>": ["+", "-", "/", "*"],
-    "<integers_>": ["<integer_>", "-<integer_>"],
+    "<integers_>": ["<integer_>", "~ <integer_>"],
     "<integer_>": ["<digit_><integer_>", "<digit_>"],
     "<digit_>": srange(string.digits),
 }
