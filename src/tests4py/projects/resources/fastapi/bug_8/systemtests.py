87d9dfe3b2e99252258abfb4af4ad8e16fd48ca5
Marius Smytzek <marius.smytzek@cispa.de>
2024-07-16 03:14:17 +0200

    fastapi

--- src/tests4py/projects/resources/fastapi/bug_8/systemtests.py
+++ src/tests4py/projects/resources/fastapi/bug_8/systemtests.py
@@ -3,63 +3,63 @@ from tests4py.tests.diversity import FailingSystemtests, PassingSystemtests
 
 class TestsFailing(FailingSystemtests):
     def test_diversity_1(self):
-        return "-p/routes/\n-mget\n-u"
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_2(self):
-        return "-p/routes/\n-mget"
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_3(self):
-        return "-p/routes/\n-mget"
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_4(self):
-        return "-p/routes/\n-o\n-mget"
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_5(self):
-        return '-p/routes/\n-o\n-mget\n-d[1,2,0.0,"test"]\n-u'
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_6(self):
-        return '-p/routes/\n-mget\n-d[1,2,0.0,"test"]'
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_7(self):
-        return "-p/routes/\n-o"
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_8(self):
-        return '-p/routes/\n-mget\n-o\n-d{"name":"test-name","price":1.6,"age":5}'
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_9(self):
-        return '-p/routes/\n-mget\n-d"test"\n-u'
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
     def test_diversity_10(self):
-        return "-p/routes/\n-mget\n-d1"
+        return "-gs /valid/ Item -a aliased_name -m get -u /valid/"
 
 
 class TestsPassing(PassingSystemtests):
     def test_diversity_1(self):
-        return "-p/router/\n-mwebsocket"
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_2(self):
-        return "-p/user/1\n-u\n-mget"
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_3(self):
-        return "-p/items/valid\n-mget"
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_4(self):
-        return '-p/custom/\n-d{"items":["first","second","third"]}\n-o'
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_5(self):
-        return '-p/items/\n-mpost\n-d{"name":"test-name","price":1.6,"age":5}'
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_6(self):
-        return '-p/items/\n-mpost\n-u\n-d{"name":"test-name","price":1.6,"age":5}'
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_7(self):
-        return '-p/items/valid\n-mget\n-o\n-d[1,2,0.0,"test"]'
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_8(self):
-        return "-p/openapi.json"
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_9(self):
-        return '-p/items/valid_list\n-mget\n-o\n-d"test"'
+        return "-gs /valid/ Item -m get -u /valid/"
 
     def test_diversity_10(self):
-        return "-p/model\n-mget\n-o\n-d1"
+        return "-gs /valid/ Item -m get -u /valid/"
